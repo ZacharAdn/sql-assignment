a6e0cc5c29e0dff7c9204d27f918383dd9cdaaa6
zaharAd <zahar91@gmail.com>
2018-10-11 17:32:17 +0300

    sql changes

--- assignment2.sql
+++ assignment2.sql
@@ -12,46 +12,47 @@ create database if not exists assignment2;
     	delimiter $$
 
 	create trigger Hebrew_ON
-	after insert on families 
+	after insert on Families
 	for each row
 	begin 
 		insert into LanguageFamilies (ID,lng) values (new.ID , 'Hebrew');
 	end; $$
 
 	delimiter ;
-	insert into families (id,familyname,phone1,phone2,cityid) values (204,"Stark",550,551,4);
 
+	insert into Soldiers (id, firstname,lastname,cityid,phone) values (101,"Zahar","Adiniaev",2,501);
+	insert into Soldiers (id, firstname,lastname,cityid,phone) values (100,"Harry","Potter",1,500);
+	insert into Soldiers (id, firstname,lastname,cityid,phone) values (102,"Ron","Vizli",3,502);
+	insert into Soldiers (id, firstname,lastname,cityid,phone) values (103,"Hermione","Granger",4,503);
+	insert into Soldiers (id, firstname,lastname,cityid,phone) values (104,"Neville","Longbottom",2,504);
+    
+	insert into Families (id,familyname,phone1,phone2,cityid) values (200,"Stark",510,511,1);
+	insert into Families (id,familyname,phone1,phone2,cityid) values (201,"Lannister",520,521,2);
+	insert into Families (id,familyname,phone1,phone2,cityid) values (202,"Targaryen",530,531,3);
+	insert into Families (id,familyname,phone1,phone2,cityid) values (203,"Baratheon",540,541,4);
+	insert into Families (id,familyname,phone1,phone2,cityid) values (203,"Bolton",540,541,4);
 
-	insert into soldiers (id, firstname,lastname,cityid,phone) values (101,"zahar","adiniaev",2,501);
-	insert into soldiers (id, firstname,lastname,cityid,phone) values (100,"tomer","hekmati",1,500);
-	insert into soldiers (id, firstname,lastname,cityid,phone) values (102,"tal","yahav",3,502);
-	insert into soldiers (id, firstname,lastname,cityid,phone) values (103,"guy","adler",4,503);
+	insert into Regions (regionid,regionname) values (01,"Tzafon");
+	insert into Regions (regionid,regionname) values (02,"Darom");
+	insert into Regions (regionid,regionname) values (03,"Merkaz");
+	insert into Regions (regionid,regionname) values (04,"Sharom");
+	insert into Regions (regionid,regionname) values (05,"Eilat");
 
-	insert into families (id,familyname,phone1,phone2,cityid) values (200,"Livni",510,511,1);
-	insert into families (id,familyname,phone1,phone2,cityid) values (201,"Mufaz",520,521,2);
-	insert into families (id,familyname,phone1,phone2,cityid) values (202,"Bibi",530,531,3);
-	insert into families (id,familyname,phone1,phone2,cityid) values (203,"Alfi",540,541,4);
-
-	insert into regions (regionid,regionname) values (01,"Kalimdor");
-	insert into regions (regionid,regionname) values (02,"Quel'thalas");
-	insert into regions (regionid,regionname) values (03,"Blackrock");
-	insert into regions (regionid,regionname) values (04,"Teldrassil");
-	insert into regions (regionid,regionname) values (05,"Lordaeron");
-
-	insert into city (cityid,cityname,regionid) values (1,"Winterfall",01);
-	insert into city (cityid,cityname,regionid) values (2,"King's landing",02);
-	insert into city (cityid,cityname,regionid) values (3,"Dorne",03);
-	insert into city (cityid,cityname,regionid) values (4,"Braavos",04);
-
-	insert into LanguageFamilies (id , lng) values (200,"English");
+	insert into City (cityid,cityname,regionid) values (1,"Winterfall",01);
+	insert into City (cityid,cityname,regionid) values (2,"Kings landing",02);
+	insert into City (cityid,cityname,regionid) values (3,"Dorne",03);
+	insert into City (cityid,cityname,regionid) values (4,"Braavos",04);
+	insert into City (cityid,cityname,regionid) values (5,"Volantis",05);
+	
+    	insert into LanguageFamilies (id , lng) values (200,"English");
 	insert into LanguageFamilies (id , lng) values (201,"Arabic");
 	insert into LanguageFamilies (id , lng) values (202,"Russian");
 	insert into LanguageFamilies (id , lng) values (203,"French");
 
 	insert into LanguageSoldiers (id , lng) values (100,"English");
 	insert into LanguageSoldiers (id, lng) values (101,"French");
-	insert into LanguageSoldiers (id, lng) values (102,"English");
+	insert into LanguageSoldiers (id, lng) values (102,"Russian");
 	insert into LanguageSoldiers (id, lng) values (103,"Arabic");
 
 
-select * from LanguageFamilies;
+select * from LanguageSoldiers;
